534daf4cb4e7db213e07d4bcba8cef83c2dca86f
Arlo Ihrig <aihrig@users.noreply.github.com>
2021-03-29 16:50:09 -0700

    Experimentation

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+// Defining and assigning a value to s for concatenation  
+var s = 'This is someone\'s string';
+s += " (?)";
+
+// Assigning HTML element values/styles
+var foo = 'bar', baz = 'bang';
+var foo = foo.fontcolor("red");
+var baz = baz.fontcolor("green");
+
+// Defining and assigning an expression
+let schmoo = 5 + 5;
+
+// Writing to the html doc
+document.write(s);
+
+// Just because
+window.alert("Why not?");
+
